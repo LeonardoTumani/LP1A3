7075ea335971274c1497cea38c99edea5530f321
Leonardo Tumani <nerdsregood@gmail.com>
2023-05-31 15:48:40 -0300

    Added Atividades

---
+++ Atividades/A1/Ave.java
@@ -0,0 +1,18 @@
+package A1;
+
+public class Ave extends Animal {
+    private String tipoBico;
+
+    public Ave (String nome, int idade, String especie, boolean alimentado, String tipoBico) {
+        super(nome, idade, especie, alimentado);
+        this.tipoBico = tipoBico;
+    }
+
+    public void emitirSom() {
+        System.out.println("Som de ave.");
+    }
+
+    public void alimentar() {
+        setAlimentado(true);
+    }
+}
---
+++ Atividades/A1/Zoologico.java
@@ -0,0 +1,25 @@
+package A1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoologico {
+    private List<Animal> listaAnimais = new ArrayList<>();
+
+    public void adicionarAnimal (Animal animal) {
+        listaAnimais.add (animal);
+    }
+
+    public void listarAnimais() {
+        for(Animal cada: listaAnimais){
+            System.out.println ("- " + cada.getNome() + ";");
+        }
+    }
+
+    public void alimentarAnimais() {
+        for(Animal cada: listaAnimais) {
+            cada.alimentar();
+        }
+
+        System.out.println("\nOs animais foram alimentados.");
+    }
+}
